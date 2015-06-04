265f4cf6fa6c95b2b04e11d3588d423baa1515e3
penging <guon1106@gmail.com>
2015-06-04 15:28:13 +0800

    Create rotatem.js

---
+++ rotatem.js
@@ -0,0 +1,26 @@
+var rotateData=[{
+	imgUrl:"http://www.penging.com/Pic/News/NEW.jpg",
+	sTit:"腥红驱逐者 奎因",
+	author:"连天坑都已经",
+	sSub:"在一次追击诺克萨斯军党的任务中，奎因由于一时疏忽导致华洛被诱杀，失去了搭档的奎因行动力大幅削弱，被炮轰致死。军团打算销毁这对来自德玛西亚的传奇搭档的残躯时，狡猾的军官斯维因发现他们的大脑都尚未死亡，于是有了诡计...",
+	picUrl:"http://lol.qq.com/act/a20141103design/works_detail.shtml?id=487"
+},{
+	imgUrl:"http://www.penging.com/Pic/News/hchllpjs1920.jpg",
+	sTit:"赤壁之怒 维迦",
+	author:"小裕先生丶",
+	sSub:"三国人物在LOL很常见啊，所以就画个三国系列的邪恶小法师吧.",
+	picUrl:"http://lol.qq.com/act/a20141103design/works_detail.shtml?id=434"
+},{
+	imgUrl:"http://www.penging.com/Pic/News/app-download.jpg",
+	sTit:"大元帅 盖伦",
+	author:"折戟君",
+	sSub:"我希望为盖伦塑造一个将领的形象，他久经沙场，力量强大而又足智多谋。卓越非凡的战绩，为他赢得了无数殊荣！",
+	picUrl:"http://lol.qq.com/act/a20141103design/works_detail.shtml?id=489"
+},{
+	imgUrl:"http://www.penging.com/Pic/News/anhuipjsqy.jpg",
+	sTit:"东海龙女 希瓦娜",
+	author:"屎命de召唤",
+	sSub:"由中国古代神话东海龙王之女而想到的创意主要表现中国风格为主，中国纹样 能代表中国风的龙。",
+	picUrl:"http://lol.qq.com/act/a20141103design/works_detail.shtml?id=294"
+},
+{}]
